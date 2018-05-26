eac506ee59daa2c9d1053570560e453d94174930
league <league@wts14-4.attlocal.net>
2018-05-26 12:31:42 -0700

    We coded today!!!

--- src/section4/DragonFight.java
+++ src/section4/DragonFight.java
@@ -12,27 +12,29 @@ public class DragonFight {
 		
 		JOptionPane.showMessageDialog(null, "Defeat the dragon to take its treasure!", "Dragon Fighter", 0, dragon);
 		// 2. Create a variable called "playerHealth" to store your health (set it equal to 100)
-	
+	            int playerhealth=100;
 		// 3. Create a variable called "dragonHealth" to store the dragon's health (set it equal to 100)
-		
+		int dragonHealth=100;
 		// 4. Create a variable to hold the damage the player's attack does each round
-		
+	int dragonhealth(100-playerdamage);
 		// 5. Create a variable to hold the damage the dragon's attack does each round
-		
-		
+		int dragondamage=22
+		int playerdamage=20;
 		// 6.  Delete the slashes at the beginning of the next line.  
-		//while(playerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
-		
+	while(playerHealth>0 && dragonHealth>0); {    //this line of code keeps the battle going until someone's health reaches 0 
+		 
 		// 7. Add a closing mustache at the very bottom of this program (since we just added an opening mustache on the previous step).
-		
+	
 		// 8. Ask the player in a pop-up if they want to attack the dragon with a yell or a kick
+		JOptionPane.showMessageDialog("Would you like to yell or kick?");
 		
 		// 9. If they typed in "yell":
 		
 			//-- Find a random number between 0 and 10 and store it in dragonDamage
-			
+			if(playerhealth<=0) { JOptionPane.showMessageDialog(null, "You Died on Your Noble Quest"){
 			//-- Subtract that number from the dragon's health variable 
 			
+		
 		// 10. If they typed in "kick":
 		
 			//-- Find a random number between 0 and 25 and store it in dragonDamage
@@ -48,18 +50,17 @@ public class DragonFight {
 		
 		
 		// 13. If the user's health is less than or equal to 0
-		
+		if(playerhealth<=0) { JOptionPane.showConfirmDialog(null, "You died");
 			//-- Tell the user that they lost
 			
 		
 		// 14. Else if the dragon's health is less than or equal to 0
-		
+		else(dragonhealth<=0){JOptionPane.sho
 			//-- Tell the user that the dragon is dead and they took a ton of gold!
 			
 	   //  15.  Else
 			
 			//-- Pop up a message that tells the their current health and the dragon's currently health (Bonus: Also display the amount of health that was lost for each player this round)
-			
+			}
 		}
-	}
-
+}
--- src/section4/RobotTreasureHunt.java
+++ src/section4/RobotTreasureHunt.java
@@ -13,53 +13,57 @@ import javax.swing.JOptionPane;
 //import org.teachingextensions.logo.robot;
 import org.jointheleague.graphical.robot.Robot;
 
-public class RobotTreasureHunt implements KeyEventDispatcher{
+public class RobotTreasureHunt implements KeyEventDispatcher {
 
 	// 1. Create a new mini robot (type "mini" inside the parentheses)
-	
-	private void goUp() throws InterruptedException {
+
+	private void goUp(mini) throws InterruptedException {
 		// 2. Make the robot move up the screen (use setAngle(angle) and microMove(distance))
 		
 	}
 
-	private void goDown() throws InterruptedException{
-		// 3. make the robot move down the screen (use setAngle(angle) and microMove(distance))
-		
-	}
-
-	private void turnLeft() throws InterruptedException{
-		// 4. Make the robot turn to the left (use setAngle(angle) and microMove(distance))
+	private void goDown() throws InterruptedException {
+		// 3. make the robot move down the screen (use setAngle(angle) and
+		// microMove(distance))
 
 	}
 
-	private void turnRight() throws InterruptedException{
-		// 5. make the robot turn to the right (use setAngle(angle) and microMove(distance))
-		
+	private void turnLeft() throws InterruptedException {
+		// 4. Make the robot turn to the left (use setAngle(angle) and
+		// microMove(distance))
+
+	}
+
+	private void turnRight() throws InterruptedException {
+		// 5. make the robot turn to the right (use setAngle(angle) and
+		// microMove(distance))
+
 	}
 
 	private void spaceBarWasPressed() {
 
-		// 5. Change ROBOTNAME below to match the name of the robot you created in step 1.  THEN, remove the slashes at the beginning of the next two lines
-		//int robotXLocation = ROBOTNAME.getX();
-		//int robotYLocation = ROBOTNAME.getY();
-		
-		// 6. Print the robotXLocation and robotYLocation variables to the console 
-		
+		// 5. Change ROBOTNAME below to match the name of the robot you created in step
+		// 1. THEN, remove the slashes at the beginning of the next two lines
+		// int robotXLocation = ROBOTNAME.getX();
+		// int robotYLocation = ROBOTNAME.getY();
+
+		// 6. Print the robotXLocation and robotYLocation variables to the console
+
 		// 7. If robot is at same location as the little girl
-		//      --make a pop-up tell the robot where to go next
-		
+		// --make a pop-up tell the robot where to go next
+
 		// 8. Give the user subsequent clues at different locations on the image
 		// (pirate robot, swamp, parrots, etc.)
-		
-		// 9.  If the robot is in the final location
-		//     --call the treasureFound() method
-		
+
+		// 9. If the robot is in the final location
+		// --call the treasureFound() method
+
 	}
 
 	private void go() {
 		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(this);
 		Robot.setWindowImage("section4/treasure_hunt.jpg");
-	
+
 		JOptionPane.showMessageDialog(null, "Ask the girl for help with your quest. Press the space bar to ask.");
 
 	}
@@ -72,8 +76,7 @@ public class RobotTreasureHunt implements KeyEventDispatcher{
 				} catch (InterruptedException e1) {
 					e1.printStackTrace();
 				}
-			}
-			else if (e.getKeyCode() == KeyEvent.VK_LEFT)
+			} else if (e.getKeyCode() == KeyEvent.VK_LEFT)
 				try {
 					turnLeft();
 				} catch (InterruptedException e2) {
@@ -96,18 +99,18 @@ public class RobotTreasureHunt implements KeyEventDispatcher{
 		}
 		return false;
 	}
-	
+
 	static void treasureFound() {
 		try {
-			URI uri = new URI("https://www.youtube.com/watch?v=G0aIg4N6aro");
+
+			uri = new URI("https://www.youtube.com/watch?v=G0aIg4N6aro");
 			java.awt.Desktop.getDesktop().browse(uri);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-
-	public static void main (String[] args) throws MalformedURLException {
+	public static void main(String[] args) throws MalformedURLException {
 		new RobotTreasureHunt().go();
 	}
 }
